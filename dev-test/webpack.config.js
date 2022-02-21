d42037be01447f0052b12cdbdabe76c82b8afe43
dd di cesare <didi@posteo.net>
2022-02-21 16:58:50 +0100

    [npm] Loaders and dependencies added

--- dev-test/webpack.config.js
+++ dev-test/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 module.exports = {
+    mode: "development",
     entry: './src/index.js',
     output: {
         filename: 'main.js',
@@ -13,4 +14,16 @@ module.exports = {
         compress: true,
         port: 9000,
     },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: "babel-loader",
+                exclude: /node_modules/,
+                options: {
+                    presets: ["@babel/preset-react"],
+                },
+            },
+        ],
+    }
 }
