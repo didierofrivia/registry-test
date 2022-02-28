43aca87332d49276b007424d34c37e35d6d83405
dd di cesare <didi@posteo.net>
2022-02-28 10:13:24 +0100

    [webpack] Webpack config and required plugins imported and configured

--- dev-test/webpack.config.js
+++ dev-test/webpack.config.js
@@ -1,8 +1,13 @@
-const path = require('path');
+const path = require('path')
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {ModuleFederationPlugin} = require("webpack").container
+const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin")
 
 module.exports = {
     mode: "development",
-    entry: './src/index.js',
+    entry: {
+        app: path.join(__dirname, "src", "index.tsx")
+    },
     output: {
         filename: 'main.js',
         path: path.resolve(__dirname, 'dist'),
@@ -21,9 +26,59 @@ module.exports = {
                 loader: "babel-loader",
                 exclude: /node_modules/,
                 options: {
-                    presets: ["@babel/preset-react"],
+                    presets: ["@babel/preset-react"]
                 },
             },
+            {
+                test: /\.(tsx|ts)?$/,
+                include: path.resolve(__dirname, "src"),
+                use: [
+                    {
+                        loader: "ts-loader",
+                        options: {
+                            transpileOnly: true,
+                            experimentalWatchApi: true,
+                        }
+                    }
+                ]
+            }
         ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./src/index.html"
+        }),
+        new ModuleFederationPlugin({
+            name: "apicurioRegistry",
+            remotes: {
+                registry: "apicurio_registry@http://localhost:8888/apicurio_registry.js"
+            },
+            shared: {
+                react: {
+                    singleton: true
+                },
+                "react-dom": {
+                    singleton: true
+                },
+                "react-router-dom": {
+                    singleton: true
+                }
+            }
+        })
+    ],
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+        plugins: [
+            new TsconfigPathsPlugin({
+                configFile: path.resolve(__dirname, "./tsconfig.json")
+            })
+        ],
+        symlinks: false,
+        cacheWithContext: false
+    },
+    performance: {
+        hints: false,
+        maxEntrypointSize: 2097152,
+        maxAssetSize: 1048576
     }
 }
